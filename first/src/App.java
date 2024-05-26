f87de03ed4c38cfd8e91b1290567d9eea6527faa
Amanvir yadav <2006489@kiit.ac.in>
2024-05-26 13:16:50 +0530

    commit message

---
+++ first/src/App.java
@@ -0,0 +1,169 @@
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        // int x=94;
+        // boolean val=true;
+        // String name="harry";
+        // float num=2.5424230f;
+        // char a='a';
+
+        // System.out.println(x);
+        // System.out.println(val);
+        // System.out.println(name);
+        // System.out.println(num);
+        // System.out.println(a);
+
+        // int x=123;
+        // int y=412;
+        // System.out.print("the sume is ");
+        // System.out.print(x+y);
+        // System.out.print(x++);
+        // System.out.print(++x);
+
+        // int age=20;
+
+        // if(age>=18)
+        // System.out.print("can vote");
+
+        // if(age<18)
+        // System.out.print("can not vote");
+
+        // else
+        // System.out.print("not valid");
+
+        //Scanner scan=new Scanner(System.in);
+        //System.out.print("enter input ");
+        // String input=scan.nextLine();
+        //String input=scan.next();
+        //System.out.print(input);
+
+        // String name="harry";
+        // String chan="code with harry";
+        // System.out.println(name);
+        // System.out.println(name+chan);
+        // System.out.println(name.length());
+        // System.out.println(name.toUpperCase());
+        // System.out.println(name.toLowerCase());
+
+        // System.out.println(name.contains("ha"));
+        // System.out.println(name.charAt(2));
+        // System.out.println(name.endsWith("ky"));
+        // System.out.println(name.indexOf("ry"));
+
+        // int x=4242;
+        // int y=3543;
+        // System.out.println(Math.max(x, y));
+        // System.out.println(Math.min(x, y));
+        // System.out.println(Math.sqrt(y));
+        // System.out.println(Math.abs(y));
+        // System.out.println(4+(10-4)*Math.random());
+        
+// Scanner scan=new Scanner(System.in);
+// System.out.println("enter day ");
+// int day=scan.nextInt();
+// System.out.print("day :");
+// System.out.println(day);
+
+// if(age<18)
+// System.out.println("you are a kid");
+
+// else if(age==18)
+// System.out.println("hey boy");
+
+// else
+// System.out.println("hey grown up");
+
+// switch(age){
+//     case 12:
+//     System.out.println("you are 12");
+//     break;
+    
+//     case 20:
+//     System.out.println("you are 20");
+//     break;
+    
+//     case 30:
+//     System.out.println("you are 30");
+//     break;
+    
+//     default:
+//     System.out.println("you are default");
+    
+// }
+
+// switch(day){
+//         case 1:
+//         System.out.println("day");
+//         break;
+
+//         case 2:
+//         System.out.println("monday");
+//         break;
+
+//         case 3:
+//         System.out.println("tuesday");
+//         break;
+
+//         case 4:
+//         System.out.println("wednesday");
+//         break;
+
+//         case 5:
+//         System.out.println("thursday");
+//         break;
+
+//         case 6:
+//         System.out.println("friday");
+//         break;
+
+//         case 7:
+//         System.out.println("satday");
+//         break;
+        
+//         default:
+//         System.out.println("you are default");
+        
+//     }
+
+// for(int i=0;i<=1000;i++){
+     // System.out.print(" ");
+//     System.out.println(i);
+// }
+
+// int x=1;
+// while(x<=100){
+//     if(x%2==0)
+//     System.out.println(x);
+//     x++;
+// }
+
+// int y=1;
+
+// do{
+//     System.out.println(y);
+//     y++;
+// }while(y<1);
+
+int [] marks={80,75,95,90,85,92};
+// System.out.println(marks[4]);
+
+// marks[4]=98;
+
+try{
+System.out.println(marks[70]);
+}
+
+catch(Exception r){
+System.out.println(r);
+}
+
+// int [][] matrix={{1,2,3,4},{1,2,3,4}};
+
+// for(int i=0;i<4;i++){
+//     for(int j=0;j<4;j++){
+//         System.out.println(matrix[i][j]);
+//     }
+// }
+
+    }
+}
